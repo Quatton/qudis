7c26a5e5e43021ff573be33a436e93e12cef28b1
Quatton <atomicatives@gmail.com>
2024-04-19 14:51:35 +0900

    feat(set): with slash

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,26 @@ async fn set(
     path: web::Path<String>,
     body: web::Bytes,
 ) -> impl Responder {
-    let key = path.into_inner();
-    let value = String::from_utf8(body.to_vec()).unwrap();
+    let path = path.into_inner();
+    let path_split = path.split('/').collect::<Vec<&str>>();
+
+    let key = path_split[0].to_string();
+    let value = if path_split.len() > 1 {
+        path_split[1].to_string()
+    } else {
+        let v = String::from_utf8(body.to_vec());
+
+        match v {
+            Ok(val) => val,
+            Err(_) => {
+                return HttpResponse::BadRequest().body("Invalid UTF-8 sequence");
+            }
+        }
+    };
+
     data.store.lock().unwrap().insert(key, value);
-    HttpResponse::Ok()
+
+    HttpResponse::Ok().body("OK".to_string())
 }
 
 fn create_app() -> App<
@@ -80,6 +96,25 @@ mod tests {
         let req = test::TestRequest::with_uri("/set/test")
             .method(Method::POST)
             .set_payload("value")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert_eq!(resp.status(), 200);
+
+        let req = test::TestRequest::with_uri("/get/test").to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert_eq!(resp.status(), 200);
+        assert_eq!(test::read_body(resp).await, "value");
+    }
+
+    #[actix_web::test]
+    async fn test_set_key_with_slash() {
+        let app = test::init_service(create_app()).await;
+        let req = test::TestRequest::with_uri("/set/test/value")
+            .method(Method::POST)
             .to_request();
 
         let resp = test::call_service(&app, req).await;
